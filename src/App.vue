cce97a896e7da6e7f1bcd3e948d862062ae3f926
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-05-14 18:44:32 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!--Author:VueSmall-->
+<script>
+import {defineComponent} from "vue";
+import content from '@/components/content.vue'
+export default defineComponent({
+    components:{
+        content
+    }
+})
+</script>
+<template>
+  <p>VueSmall</p>
+  <content></content>
+</template>
+<style>
+*{
+    text-align: center;
+}
+</style>
